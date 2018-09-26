49ef984020cfe6fb57930adb51f42ceffb89884e
zlikun <zlikun-dev@hotmail.com>
2018-09-26 15:48:10 +0800

    初始化工程

---
+++ src/main/java/com/zlikun/open/OpenAsyncNotifyApplication.java
@@ -0,0 +1,12 @@
+package com.zlikun.open;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenAsyncNotifyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenAsyncNotifyApplication.class, args);
+    }
+}
